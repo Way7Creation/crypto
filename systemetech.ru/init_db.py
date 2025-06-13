4eee960baa70a5f9a44e5a1c15417d7a2f48b1f7
Way Sen <86974397+Way7Creation@users.noreply.github.com>
2025-06-13 11:41:14 +0300

    Delete systemetech.ru directory

--- systemetech.ru/init_db.py
+++
@@ -1,7 +0,0 @@
-# init_db.py - запустите этот файл один раз для создания таблиц
-from src.core.database import engine
-from src.core.models import Base
-
-print("Создание таблиц...")
-Base.metadata.create_all(bind=engine)
-print("Таблицы созданы успешно!")
